a8410b6f646f75af756f293adc2ec5d6e74cc503
Estudiante <justinvargas4299@gmail.com>
2023-09-22 19:45:14 -0600

    Main, caso01.py y caso02.py Terminados.

---
+++ caso01.py
@@ -0,0 +1,10 @@
+""" Use the pass keyword when you do not want to
+ add any other properties or methods to the class"""
+
+
+class Persona:
+    pass
+
+
+if __name__ == '__main__':
+    person = Persona()
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-""" Use the pass keyword when you do not want to
- add any other properties or methods to the class"""
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-class Persona:
-    pass
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    person = Persona()
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
